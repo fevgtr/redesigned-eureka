7388de8e9ca0e385a28690fa0750223de3655dfc
fevgtr <39020211+fevgtr@users.noreply.github.com>
2020-05-08 10:25:30 +0800

    Create P1001.cpp

---
+++ P1001.cpp
@@ -0,0 +1,12 @@
+//This file is from redesigned-eureka
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin>>a>>b;
+	c=a+b;
+  printf("%d",c);
+	return 0;
+}
